ad9486253bf67083b89480e4f629bb3748861e66
favour777 <favourmbata520@gmail.com>
2023-06-26 12:16:08 +0100

    python application

---
+++ chapter3/exercise3_3.py
@@ -0,0 +1,8 @@
+def sum1(number):
+    total= 0
+    for counter in range(1,number+1):
+        total += counter
+    print(total)
+sum1(10)
+sum1(10)
+sum1(30)
---
+++ chapter3/multiplication_table.py
@@ -0,0 +1,6 @@
+for i in range(1,13):
+        print()
+        for k in range(1,13):
+            print(f"{i:>3} x {k:>3} = {i * k:>3} ",end='\t\t')
+
+
