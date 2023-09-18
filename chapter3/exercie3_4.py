5f0fc1279902363fec1bd1c59272e7cd5e8fcedb
favour777 <favourmbata520@gmail.com>
2023-09-18 10:39:26 +0100

    favour

--- chapter3/exercie3_4.py
+++
@@ -1,5 +0,0 @@
-for row in range(2):
-    for row in range(7):
-        print('@',end=' ')
-      
-    
---
+++ chapter3/missing_codw.py
@@ -0,0 +1,9 @@
+# for row in range(7):
+#     for row in range(7):
+#         print('@',end=' ')
+#         print()
+      
+for row in  range (7):
+ print("@",end= " ")
+ print("@",end= " ")
+ print()
