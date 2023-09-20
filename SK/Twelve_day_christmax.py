322588b2a5e436a642d252f3039b3dac90991d1b
favour777 <favourmbata520@gmail.com>
2023-09-20 09:54:35 +0100

    favour

--- SK/Twelve_day_christmax.py
+++ SK/Twelve_day_christmax.py
@@ -1,24 +1,31 @@
-christmax_song = input("""
-                press 1 to print the first day of christmax
-                press 2 to print the second day of christmax
-                press 3 to print the third day of christmax:
-                press 4 to print the fourth day of christmax:
-                press 5 to print the fifth day of christmax:
-                press 6 to print the sixth day of christmax:
-                press 7 to print the seventh day of christmax:
-                 press 8 to print th eighth day of christmax:
-                press 9 to print the nineth day of christmax:
-                press 10 to print the tenth day of christmax: 
-                press 11 to print the eleventh day of christmax:
-                press 12 to print the twelveth day of christmax:
-                press 0 to quit
-                         """)
-match christmax_song:
-    case "on the first day of christmax ,my true love gave to me":
-        print("A patridge in a pear tree")
-    case "on the second day of christmax,my true love gave to me:":
-        print(f"Two turtle dove and \nA patridge in a pear tree: ")
-    case "On the third day of christmax,my true love gave to me:":
-        print(f"Three french hens\n two turtle dove \n And A patridge in a pear tree")
-    case "On the fourth day of christ max,my true love gave to me":
-      print(f"Four calling birds \nThree french hens\ntwo turtle dove\n And A patridge in a pear tree")
+day    =     ("""
+                1:firstDay
+                2:secondDay
+                3:third_day
+                4:fourth_day
+                5:fifthday
+                6:sixth_day
+                7:seventh_day
+                8:eight_day
+                9:ninth_day
+                10:tenth_day
+                11:eleventh_day
+                12:twelfth_day
+              
+                  """)
+
+days_of_christmax = {
+    1: "a_partridge_in_pear_tree",
+    2: " Two turtle doves, and",
+    3: "Three french hens",
+    4: "Four calling birds",
+    5: "Five golden rings",
+    6: "six calling birds",
+    7: "seven swans_a swimming",
+    8: "Eight maids a milking",
+    9: "Nine ladies dancing",
+    10: "Ten lords a leaping",
+    11: "Eleven pipers piping",
+    12: "Twelve drummers drumming"
+
+    }
--- sk_task/Print_0_6.py
+++ sk_task/Print_0_6.py
@@ -6,18 +6,18 @@ for i in range (6):
         continue
   print(i,end=" ")
 
-
-number = 0
-while number <= 6:
-    if number != 3 and number != 6:
-        print(number,end=" ")
-    number +=1
-
-
-
-for i in range (6):
-    if i != 3 and i != 6:
-        print(i,end=" ")
-
-for i in range(10):
-     print(i**2)
+#
+# number = 0
+# while number <= 6:
+#     if number != 3 and number != 6:
+#         print(number,end=" ")
+#     number +=1
+#
+#
+#
+# for i in range (6):
+#     if i != 3 and i != 6:
+#         print(i,end=" ")
+#
+# for i in range(10):
+#      print(i**2)
